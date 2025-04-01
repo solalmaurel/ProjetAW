89c6ca3e21da5add9b2fcb33dbf441b6885e18db
Pierre SAUSSEREAU <pierre.saussereau@etu.univ-tours.fr>
2025-04-01 17:29:20 +0200

    add evenement

--- entraide/src/main/java/com/entraide/model/Evenement.java
+++ entraide/src/main/java/com/entraide/model/Evenement.java
@@ -1,4 +1,109 @@
 package com.entraide.model;
 
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Date;
+import java.util.List;
+
+@Entity
 public class Evenement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long idEvenement;
+    private boolean isOnline;
+    private Date dateDebut;
+    private Date dateFin;
+    private Theme theme;
+    private float prixNormal;
+    private float prixAdherent;
+    private String description;
+
+    @OneToMany
+    Adresse adresse;
+
+    @ManyToMany
+    List<User> utilisateurs;
+
+    public Evenement(Long idEvenement) {
+
+        this.idEvenement = idEvenement;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public void addUtilisateur(User user) {
+        this.utilisateurs = user;
+    }
+
+
+    public Long getIdEvenement() {
+        return idEvenement;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    public DateTimeFormat getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(DateTimeFormat dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public DateTimeFormat getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(DateTimeFormat dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+
+    public float getPrixNormal() {
+        return prixNormal;
+    }
+
+    public void setPrixNormal(float prixNormal) {
+        this.prixNormal = prixNormal;
+    }
+
+    public float getPrixAdherent() {
+        return prixAdherent;
+    }
+
+    public void setPrixAdherent(float prixAdherent) {
+        this.prixAdherent = prixAdherent;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public List<User> getUtilisateurs() {
+        return utilisateurs;
+    }
 }
---
+++ entraide/src/main/java/com/entraide/model/Theme.java
@@ -0,0 +1,5 @@
+package com.entraide.model;
+
+public enum Theme {
+
+}
