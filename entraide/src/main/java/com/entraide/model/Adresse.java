3f4225e95e4288a6c712d60a080e8bbeb246974c
Daphné Navratil <daphnenavratil@gmail.com>
2025-04-01 17:46:09 +0200

    Creation entity entreprise et ajout getters setters dans Adresse

--- entraide/src/main/java/com/entraide/model/Adresse.java
+++ entraide/src/main/java/com/entraide/model/Adresse.java
@@ -29,6 +29,70 @@ public class Adresse {
     @OneToMany(mappedBy = "adresse")
     private List<Evenement> evenements;
 
+    public Long getIdAdresse() {
+        return idAdresse;
+    }
+
+    public void setIdAdresse(Long idAdresse) {
+        this.idAdresse = idAdresse;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public Etablissement getEtablissement() {
+        return etablissement;
+    }
+
+    public void setEtablissement(Etablissement etablissement) {
+        this.etablissement = etablissement;
+    }
+
+    public List<Evenement> getEvenements() {
+        return evenements;
+    }
+
+    public void setEvenements(List<Evenement> evenements) {
+        this.evenements = evenements;
+    }
+
 
 
 
--- entraide/src/main/java/com/entraide/model/Entreprise.java
+++ entraide/src/main/java/com/entraide/model/Entreprise.java
@@ -1,4 +1,69 @@
 package com.entraide.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Entreprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEntreprise;
+    private String nom;
+    
+    @ManyToOne
+    @JoinColumn(name = "idAdresse")
+    private Adresse adresse;
+    
+    @OneToMany(mappedBy = "entreprise")
+    private List<Offre> offres;
+
+    public Entreprise(Long idEntreprise) {
+        this.idEntreprise = idEntreprise;
+    }
+
+    public Entreprise(Long idEntreprise, String nom, Adresse adresse, List<Offre> offres) {
+        this.idEntreprise = idEntreprise;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.offres = offres;
+    }
+
+    public Long getIdEntreprise() {
+        return idEntreprise;
+    }
+
+    public void setIdEntreprise(Long idEntreprise) {
+        this.idEntreprise = idEntreprise;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public List<Offre> getOffres() {
+        return offres;
+    }
+
+    public void setOffres(List<Offre> offres) {
+        this.offres = offres;
+    }
 }
