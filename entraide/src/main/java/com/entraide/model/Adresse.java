94dceba0c65ad766119b1251ba628bf69ce3cede
Pierre SAUSSEREAU <pierre.saussereau@etu.univ-tours.fr>
2025-04-01 17:39:22 +0200

    add evenement

--- entraide/src/main/java/com/entraide/model/Adresse.java
+++ entraide/src/main/java/com/entraide/model/Adresse.java
@@ -1,4 +1,18 @@
 package com.entraide.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Adresse {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
