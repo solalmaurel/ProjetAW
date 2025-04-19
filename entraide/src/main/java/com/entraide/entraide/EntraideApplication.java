9f7673761c0163d3f6b82819b1283d071c740478
Alexandre P <alexandre.perrot54@proton.me>
2025-04-19 12:18:24 +0200

    update backend

--- entraide/src/main/java/com/entraide/entraide/EntraideApplication.java
+++ entraide/src/main/java/com/entraide/entraide/EntraideApplication.java
@@ -2,12 +2,35 @@ package com.entraide.entraide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EntraideApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntraideApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.csrf(AbstractHttpConfigurer::disable);
+		return http.build();
 	}
 
 }
