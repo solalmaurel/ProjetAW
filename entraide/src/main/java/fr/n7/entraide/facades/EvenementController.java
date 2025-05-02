686d01740a23cca16cb2b32bad46c776760aa8c7
Daphné Navratil <daphnenavratil@gmail.com>
2025-05-02 10:46:45 +0200

    creation repository et controller Evenement

---
+++ entraide/src/main/java/fr/n7/entraide/facades/EvenementController.java
@@ -0,0 +1,44 @@
+package fr.n7.entraide.facades;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.n7.entraide.entities.Evenement;
+import fr.n7.entraide.repositories.EvenementRepository;
+import fr.n7.entraide.utils.ResponseHandler;
+
+@RestController
+@RequestMapping("/evenement")
+public class EvenementController {
+    @Autowired
+    private EvenementRepository evenementRepository;
+
+    @GetMapping
+    public List<Evenement> getAllEvenements() {
+        return evenementRepository.findAll();
+    }
+
+    @PostMapping(path = "/create", produces=MediaType.APPLICATION_JSON_VALUE ) 
+    public ResponseEntity<Object> createEvenement(@RequestBody Evenement evenement) {
+        evenementRepository.save(evenement);
+        return ResponseHandler.generateResponse("Evenement created successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteOffer(@PathVariable("id") Long id) {
+        evenementRepository.deleteById(id);
+        return ResponseHandler.generateResponse("Offer deleted successfully", HttpStatus.OK);
+    }
+}
