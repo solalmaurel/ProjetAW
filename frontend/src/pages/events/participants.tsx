fb932c87028be00542db229ad93936997bbd43c6
Daphné Navratil <daphnenavratil@gmail.com>
2025-05-14 13:24:16 +0200

    Apparence du tableau pour les participants d'un evenement améliorée

--- frontend/src/pages/events/participants.tsx
+++ frontend/src/pages/events/participants.tsx
@@ -33,8 +33,42 @@ export default function ParticipantsPage() {
 
     return (
         <div>
-            <h2>Liste des participants</h2>
-            <table>
+            <h1 className="text-2xl"> Liste des participants </h1>
+            <style>
+                {`
+                    .styled-table {
+                        border-collapse: collapse;
+                        margin: 25px 0;
+                        font-size: 0.9em;
+                        font-family: sans-serif;
+                        min-width: 400px;
+                        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+                    }
+                    .styled-table thead tr {
+                        background-color: #1E90FF;
+                        color: #ffffff;
+                        text-align: left;
+                    }
+                    .styled-table th,
+                    .styled-table td {
+                        padding: 12px 15px;
+                    }
+                    .styled-table tbody tr {
+                        border-bottom: 1px solid #dddddd;
+                    }
+                    .styled-table tbody tr:nth-of-type(even) {
+                        background-color: #f3f3f3;
+                    }
+                    .styled-table tbody tr:last-of-type {
+                        border-bottom: 2px solid #1E90FF;
+                    }
+                    .styled-table tbody tr.active-row {
+                        font-weight: bold;
+                        color:solid #1E90FF;
+                    }
+                `}
+            </style>
+            <table className="styled-table">
                 <thead>
                     <tr>
                         <th>Nom</th>
